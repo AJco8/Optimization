7134e9b8beea8330098164d0e80abf775cd62580
AJ <64288654+AJco8@users.noreply.github.com>
2025-12-19 20:17:04 -0500

    Removed irrelevant files

--- Project 4/Fig/compare.py
+++
@@ -1,9 +0,0 @@
-file = open('light.txt').read()
-light = [i.strip for i in file.split('\n')]
-file = open('dark.txt').read()
-dark = [i.strip for i in file.split('\n')]
-
-for i in range(len(dark)):
-    if dark[i] != light[i]:
-        print(i)
-    
